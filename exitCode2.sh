f147449e82c3a10dae75aec48f6c12faf61964aa
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-05-04 14:20:03 +0530

    update.sh

--- exitCode2.sh
+++ exitCode2.sh
@@ -1,13 +1,54 @@
 #!/bin/bash
 
-package=htop
+# package=htop
 
-sudo apt install $package >> package_install_result.log
+# sudo apt install $package >> package_install_result.log
 
-if [ $? -eq 0 ]
+# if [ $? -eq 0 ]
+# then 
+# 	echo "Installation was success"
+# 	which $package
+# else
+# 	echo "The installation failed" >> package_install_failure.log
+# fi
+
+
+# ----------------------------------------
+
+
+# directory=/notexist
+# if [ -d $directory ]
+# then 
+# 	# echo $?
+# 	echo "The directory $directory exists"
+
+# else
+# 	# echo $?
+#     echo "The directory $directory does not exists"
+# fi
+
+# echo "The exit code for script is $?"
+
+# # ----------------------------------------
+
+
+# echo "Hello World"
+# exit 1
+# echo $?
+
+
+# ----------------------------------------
+
+
+directory=/notexist
+if [ -d $directory ]
 then 
-	echo "Installation was success"
-	which $package
+	echo "The directory $directory exists"
+	exit 0
+
 else
-	echo "The installation failed" >> package_install_failure.log
+    echo "The directory $directory does not exists"
+	exit 199
 fi
+echo "The exit code for script is $?"
+echo "You won't see this"
