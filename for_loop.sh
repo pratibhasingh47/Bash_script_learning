2dca765409a141a1668c54c014e95849a867f10d
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-05-04 22:49:18 +0530

    update.sh

--- for_loop.sh
+++ for_loop.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# for num in 1 2 3 4 5 6 7 8 9 10
-# for num in {1..10}
-# do
-# 	echo $num
-# 	sleep 0.5
-# done
+for num in 1 2 3 4 5 6 7 8 9 10
+for num in {1..10}
+do
+	echo $num
+	sleep 0.5
+done
 
-# echo "The loop ends"
+echo "The loop ends"
 
 
 # -------------------------------
