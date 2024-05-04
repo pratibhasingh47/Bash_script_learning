89509f82397f7469e590004711ebaf10e683e8e4
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-05-04 22:47:28 +0530

    update.sh

---
+++ for_loop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# for num in 1 2 3 4 5 6 7 8 9 10
+# for num in {1..10}
+# do
+# 	echo $num
+# 	sleep 0.5
+# done
+
+# echo "The loop ends"
+
+
+# -------------------------------
+
+for file in logfiles/*.log
+do 
+	tar -czvf $file.tar.gz $file
+done
